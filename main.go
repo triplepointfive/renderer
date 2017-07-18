ca25494828c57437ebae974cb09d82c49e417987
Ilya Smelkov <ismelkov@restream.rt.ru>
2017-07-18 11:22:16 +0300

    Load and draw obj 🍝

--- main.go
+++ main.go
@@ -4,9 +4,14 @@ import (
 	"image"
 	"image/color"
 	"math"
+	"os"
 
 	"github.com/llgcode/draw2d/draw2dimg"
+	"github.com/sheenobu/go-obj/obj"
 )
+
+const width = 400
+const height = 400
 
 func line(r *image.RGBA, x0, y0, x1, y1 float64, color color.Color) {
 	steep := false
@@ -50,15 +55,36 @@ var red = color.RGBA{0xff, 0x00, 0x00, 0xff}
 var white = color.RGBA{0xff, 0xff, 0xff, 0xff}
 
 func main() {
-	dest := image.NewRGBA(image.Rect(0, 0, 100, 100))
+	dest := image.NewRGBA(image.Rect(0, 0, width, height))
 	gc := draw2dimg.NewGraphicContext(dest)
 
 	gc.SetFillColor(color.RGBA{0x00, 0x00, 0x00, 0xff})
 	gc.SetStrokeColor(color.RGBA{0xff, 0x00, 0x00, 0xff})
 	gc.Clear()
 
-	line(dest, 13, 20, 80, 40, white)
-	line(dest, 20, 13, 40, 80, red)
-	line(dest, 80, 40, 13, 20, red)
+	f, err := os.Open("tinyrenderer/obj/african_head/african_head.obj")
+	if err != nil {
+		panic(err)
+	}
+
+	head, err := obj.NewReader(f).Read()
+	if err != nil {
+		panic(err)
+	}
+
+	for _, face := range head.Faces {
+		for i, point := range face.Points {
+			v0, v1 := point.Vertex, face.Points[(i+1)%3].Vertex
+			line(
+				dest,
+				(v0.X+1)*width/2,
+				(v0.Y+1)*height/2,
+				(v1.X+1)*width/2,
+				(v1.Y+1)*height/2,
+				white,
+			)
+		}
+	}
+
 	draw2dimg.SaveToPngFile("watcher/hello.png", dest)
 }
